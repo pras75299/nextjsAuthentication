39ff063a43c0177e695a8b8db1f7ff6d89877372
pras75299 <singh.prashantking@gmail.com>
2024-04-04 00:22:23 +0530

    logout route complete

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+
+export async function GET(){
+    try {
+        const response = await NextResponse.json(
+            {
+                message: "Logout Successful",
+                success: true
+            }
+        )
+        response.cookies.set("token", "", {
+            httpOnly: true,
+            expires: new Date(0)
+        });
+        return response;
+    } catch (error:any) {
+        return NextResponse.json(
+            {error: error.message},
+            {status: 500}
+            )
+    }
+}
