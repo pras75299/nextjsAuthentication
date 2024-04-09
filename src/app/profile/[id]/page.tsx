df30f3eb9c1f897a8140d116d8e6eb10a8211d37
pras75299 <singh.prashantking@gmail.com>
2024-04-10 02:27:54 +0530

    update layout and css to main page.tsx and profile/[id]

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -2,9 +2,13 @@ import React from "react";
 
 const ProfilePage = ({ params }: any) => {
   return (
-    <div>
-      ProfilePage <span className="bg-red-400">{params.id}</span>
-    </div>
+    <section className="bg-gradient-to-r from-blue-500 to-purple-600 min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="mb-4 text-white text-3xl font-bold">
+          ProfilePage <span className="bg-red-400">{params.id}</span>
+        </h1>
+      </div>
+    </section>
   );
 };
 
